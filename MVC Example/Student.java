4e41ab6857f80ba233c149f29590495da62cacc7
ktjszrena <147378674+ktjszrena@users.noreply.github.com>
2023-10-20 09:28:00 +0800

    Add files via upload

---
+++ MVC Example/Student.java
@@ -0,0 +1,26 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Student {
+    public String rollNo;
+    private String name;
+
+    public String getRollNo()
+    {
+        return rollNo;
+    }
+
+    public void setRollNo(String rollNo)
+    {
+        this.rollNo = rollNo;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
