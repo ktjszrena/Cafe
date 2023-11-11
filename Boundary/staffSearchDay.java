59dd35c27ac79e3447e8712f9cd3e19ab67f829f
KotoyoChan <36572857+KotoyoChan@users.noreply.github.com>
2023-11-11 13:48:03 +0800

    Add files via upload

--- Boundary/staffSearchDay.java
+++ Boundary/staffSearchDay.java
@@ -35,7 +35,7 @@ public class staffSearchDay extends javax.swing.JFrame {
         slotsResultLabel = new javax.swing.JLabel();
         searchButton = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setText("Date:");
 
--- Boundary/staffSlotView.java
+++ Boundary/staffSlotView.java
@@ -37,7 +37,7 @@ public class staffSlotView extends javax.swing.JFrame {
         searchButton = new javax.swing.JButton();
         viewButton = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setText("Date:");
 
